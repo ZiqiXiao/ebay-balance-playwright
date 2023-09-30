078cef1aacd1761723741b9bfa364f18ed08b351
Ziqi Xiao <xzqshawn@126.com>
2023-09-30 10:34:15 +0000

    Adjustment

--- nginx-setup.sh
+++ nginx-setup.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt install -y nginx
 sudo rm -rf /etc/nginx/nginx.conf
-sudo ln -s /workspaces/python/nginx.conf /etc/nginx/nginx.conf
+sudo ln -s /workspaces/ebay-balance/nginx.conf /etc/nginx/nginx.conf
 sudo service nginx start
