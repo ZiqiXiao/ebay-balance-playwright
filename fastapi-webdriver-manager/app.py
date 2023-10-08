2f1cc3a2d46977785e6b94fd6c7010af51dea3f6
Ziqi Xiao <xzqshawn@126.com>
2023-10-08 08:47:14 +0000

    change redis config

--- fastapi-webdriver-manager/app.py
+++ fastapi-webdriver-manager/app.py
@@ -16,13 +16,14 @@ from playwright_crawl.utils.scheduler import Scheduler
 
 PORT_LIST = os.environ.get('PORTS', '').split(',')
 LOG_NAME = os.environ.get('LOG_NAME', 'logs')
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 
 logger.add(os.path.join('logs', LOG_NAME), rotation="1 day", retention="7 days", level='DEBUG')
 
 PORT_DISABLED_INTERVAL = 1200
 PORT_RENEW_INTERVAL = PORT_DISABLED_INTERVAL + 10
 
-r = Redis(db=0, decode_responses=True)
+r = Redis(host=REDIS_HOST, db=0, decode_responses=True)
 
 
 async def run_command(cmd):
