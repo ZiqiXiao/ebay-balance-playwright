6202194d9dcc416a91856da9882d050452aa94df
Ziqi Xiao <xzqshawn@126.com>
2023-10-11 02:47:13 +0000

    Update

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 #uvicorn --workers 4 "fastapi-webdriver-manager.app:app" --reload
 #uvicorn --workers 1 "fastapi-webdriver-manager.app:app"
 
-# PORTS="16600,16601" LOG_NAME='group0.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --port 9560 &
+# PORTS="16600" LOG_NAME='group0.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --port 9560
 
 PORTS="16600,16601" LOG_NAME='group0.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --host 0.0.0.0 --port 9560 &
 PORTS="16610,16611" LOG_NAME='group1.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --host 0.0.0.0 --port 9561 &
