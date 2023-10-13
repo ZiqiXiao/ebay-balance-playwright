eeaa11a4ad726e250695f893bbd51aca8f241583
Ziqi Xiao <xzqshawn@126.com>
2023-10-13 09:49:55 +0000

    Update

--- playwright_crawl/utils/mission/login.py
+++ playwright_crawl/utils/mission/login.py
@@ -75,7 +75,8 @@ class LoginMission(object):
             address[1] = address[1][0:4]
         address = ' '.join(address)
         address = '5th'
-        await address_input.press_sequentially(self.page.locator('input[id="addressSugg"]'), delay=random.uniform(50, 150))
+        address_locator = await self.page.get_by_placeholder('Enter your street address')
+        await .press_sequentially(address, delay=random.uniform(50, 150))
         logger.debug('Address Filled')
 
         while True:
