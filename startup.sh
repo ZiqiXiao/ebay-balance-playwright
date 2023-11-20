d1126eb58d4f87dde9ffcbd9a6a32c43763c2afc
Ziqi Xiao <xzqshawn@126.com>
2023-11-20 14:24:50 +0000

    Update

--- startup.sh
+++ startup.sh
@@ -4,9 +4,9 @@
 
 # PORTS="16600" LOG_NAME='group0.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --port 9560
 
-PORTS="16600,16601" LOG_NAME='group0.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --host 0.0.0.0 --port 9560 &
-PORTS="16610,16602" LOG_NAME='group1.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --host 0.0.0.0 --port 9561 &
-# PORTS="16602,166003" LOG_NAME='group2.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --host 0.0.0.0 --port 9562 &
+PORTS="16600,16610" LOG_NAME='group0.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --host 0.0.0.0 --port 9560 &
+PORTS="16601,16611" LOG_NAME='group1.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --host 0.0.0.0 --port 9561 &
+# PORTS="16602,16612" LOG_NAME='group2.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --host 0.0.0.0 --port 9562 &
 # PORTS="16603,16613" LOG_NAME='group3.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --host 0.0.0.0 --port 9563 &
 #PORTS="16604,16614" LOG_NAME='group4.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --host 0.0.0.0 --port 9564 &
 #PORTS="16605,16615" LOG_NAME='group5.log' uvicorn "fastapi-webdriver-manager.app:app" --workers 1 --host 0.0.0.0 --port 9565 &
